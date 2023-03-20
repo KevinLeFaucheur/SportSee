646895288a84eddda98786bfe45952a825e1570e
Kevin Le Faucheur <lefaucheur.ke@gmail.com>
2023-03-20 19:26:05 +0100

    Loading component base

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,7 +1,17 @@
 import React from 'react'
+import styled from 'styled-components'
+
+const LoadingWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+`
 
 export const Loading = () => {
   return (
-    <div>Loading</div>
+    <LoadingWrapper>
+      <p>Loading</p>
+    </LoadingWrapper>
   )
 }
